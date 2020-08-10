b32c6d9a286e9660fee6f70b0cdbd15fa9b2ebe8
zeyadhossam <51086000+zeyadhossam@users.noreply.github.com>
2020-08-10 15:12:50 +0200

    adding starting project

---
+++ script.js
@@ -0,0 +1,12 @@
+const name = "james";
+
+const person = {
+  fisr: name,
+};
+console.log(person);
+
+const sayHelloLintinf = (fname) => {
+  console.log(`hello linting,${fname}`);
+};
+
+sayHelloLintinf("james");
